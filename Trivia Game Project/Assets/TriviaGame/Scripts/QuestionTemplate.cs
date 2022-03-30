ddf007bda58e3296005befa4313cbd8391a62b15
Yaroslav Vorozhbyt <yaroslav.vorozhbyt@lindenvalley.de>
2022-03-30 21:29:18 +0200

    Start generate Questions

--- Trivia Game Project/Assets/TriviaGame/Scripts/QuestionTemplate.cs
+++ Trivia Game Project/Assets/TriviaGame/Scripts/QuestionTemplate.cs
@@ -23,13 +23,39 @@ namespace TriviaGame.Scripts
         PresetAnswers
     }
 
-
-    [Serializable]
-    public class QuestionData <Q, A>
+    public interface IQuestionData
     {
-        public SequenceQuestion Type;
-        public QuestionTemplate<Q, A>[] Questions;
-        public List<Sprite> _answers;
+        void SetTotalQuestionInLevel(int count);
+        float Quantity { get; set; }
+        int QuestionCount { get; }
+        SequenceQuestion Type { get; }
     }
 
+
+    [Serializable]
+    public class QuestionData<Q, A> : IQuestionData
+    {
+        [SerializeField] private SequenceQuestion _type;
+        public QuestionTemplate<Q, A>[] Questions;
+        public List<Sprite> Answers;
+
+        private int _quantity;
+        private const int ToPersentage = 100;
+        private int _questionToShow;
+
+        public void SetTotalQuestionInLevel(int count)
+        {
+            _questionToShow = (int) Math.Round(Quantity * count);
+        }
+
+        public float Quantity
+        {
+            set => _quantity = (int) Math.Round((double) (Questions.Length / value * ToPersentage));
+
+            get => _quantity;
+        }
+
+        public int QuestionCount => Questions.Length;
+        public SequenceQuestion Type => _type;
+    }
 }
--- Trivia Game Project/Assets/TriviaGame/Scripts/QuestionsLevelController.cs
+++ Trivia Game Project/Assets/TriviaGame/Scripts/QuestionsLevelController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TriviaGame.Scripts;
 using UnityEngine;
 
@@ -8,9 +9,33 @@ public class QuestionsLevelController : MonoBehaviour
     [SerializeField] private ImageQuestionLevel _imageQuestionLevel;
     [SerializeField] private TextImageQuestionLevel _textImageQuestionLevel;
     [SerializeField] private TrueFalseQuestionLevel _trueFalseQuestionLevel;
+    [SerializeField] private int _questionInLevel = 10;
     
+    private List<IQuestionData> _questionData = new List<IQuestionData>();
 
-    void Start()
+    private async void Start()
     {
+        int numberOfTotalQuestion = 0;
+        _questionData.Add(_questionsOrder.TextQuestion);
+        _questionData.Add(_questionsOrder.ImageQuestion);
+        _questionData.Add(_questionsOrder.TextImageQuestion);
+        _questionData.Add(_questionsOrder.TrueFalseQuestion);
+        _questionData.Add(_questionsOrder.BordersQuestion);
+        _questionData.Add(_questionsOrder.UrbanPopulationQuestion);
+        _questionData.Add(_questionsOrder.PopulationLowQuestion);
+        _questionData.Add(_questionsOrder.PopulationMidQuestion);
+        _questionData.Add(_questionsOrder.PopulationHighQuestion);
+        
+        foreach (var question in _questionData)
+        {
+            numberOfTotalQuestion += question.QuestionCount;
+        }
+
+        foreach (var question in _questionData)
+        {
+            question.Quantity = numberOfTotalQuestion;
+            question.SetTotalQuestionInLevel(_questionInLevel);
+        }
+        
     }
 }
